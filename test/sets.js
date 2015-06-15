d56f7c00c12739d011e05495b801d4509e049d38
Ivo Julca <ivojulca@hotmail.com>
2015-06-15 00:34:34 -0500

    Implement automatized builds

---
+++ test/sets.js
@@ -0,0 +1,8 @@
+var assert = require('assert');
+var builder = require('./..');
+
+describe("Collected data", function () {
+	it("should not have any error", function (done) {
+		builder.run(done);
+	});
+});
